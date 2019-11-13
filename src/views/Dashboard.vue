d23e88b4073552ac845e10d7ea7cdecd7ba523f6
Marcin <m.stolvrczyk@gmail.com>
2019-11-13 20:36:53 +0100

    initial commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+<v-container fluid>
+  <v-row>
+    <v-col cols="6" offset="3">
+      <v-card height="200" color="#9FA8DA"></v-card>
+    </v-col>
+  </v-row>
+</v-container>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
+
+<style>
+
+</style>
