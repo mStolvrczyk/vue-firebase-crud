af5a6499b1c1387381b404d436e82044752b0121
Marcin Stolarczyk <m.stolvrczyk@gmail.com>
2019-11-14 09:28:43 +0100

    working on layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <v-app>
+    <Navbar/>
     <v-content>
-      <router-view/>
+      <v-container class="fill-height">
+        <router-view/>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
 
+import Navbar from './components/ui/Navbar'
+// import View from './components/ui/View'
 export default {
   name: 'App',
+  components: { Navbar },
   data: () => ({
     //
   })
---
+++ src/components/ui/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar
+    app
+    color="indigo"
+    dark
+  >
+    <v-toolbar-title>Application</v-toolbar-title>
+  </v-app-bar>
+
+</template>
+
+<script>
+export default {
+  name: 'Drawer'
+}
+</script>
+
+<style scoped>
+
+</style>
